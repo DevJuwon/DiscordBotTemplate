1f5ac3d86e011dab66861887cde4cf91908c3063
MakerZip <58218300+DevJuwon@users.noreply.github.com>
2021-02-11 18:40:47 +0900

    Update Config.java

--- src/main/java/com/makerzip/template/Config.java
+++ src/main/java/com/makerzip/template/Config.java
@@ -12,17 +12,17 @@ public class Config {
         Properties prop = new Properties();
         URL url = Thread.currentThread().getContextClassLoader().getResource("token.properties");
         if (debug) {
+            try {
+                prop.load(url.openStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             if (prop.getProperty("test-token").length() == 59) {
-                try {
-                    prop.load(url.openStream());
-                } catch (IOException e) {
-                    System.err.println("You did not write down the test-token. So it works as the main-token");
-                }
                 return prop.getProperty("test-token");
             } else {
+                System.err.println("You did not write down the test-token. So it works as the main-token");
                 return prop.getProperty("main-token");
             }
-
         } else {
             try {
                 prop.load(url.openStream());
